9107c3aff478f4168d21ee4a1efc8df9bdc8652f
Denison G. Corbal <denison.corbal@gmail.com>
2023-07-01 09:49:46 -0300

    java spring base as backend

---
+++ backend/src/main/java/com/dgc/backendonlineekklisia/BackendOnlineEkklisiaApplication.java
@@ -0,0 +1,13 @@
+package com.dgc.backendonlineekklisia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOnlineEkklisiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOnlineEkklisiaApplication.class, args);
+	}
+
+}
