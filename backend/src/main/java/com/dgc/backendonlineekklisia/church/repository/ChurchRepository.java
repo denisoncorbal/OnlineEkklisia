ca9233b93faf537f31cd973a0626cea5c6c54cff
Denison G. Corbal <denison.corbal@gmail.com>
2023-07-30 15:25:16 -0300

    backend create church

---
+++ backend/src/main/java/com/dgc/backendonlineekklisia/church/repository/ChurchRepository.java
@@ -0,0 +1,9 @@
+package com.dgc.backendonlineekklisia.church.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dgc.backendonlineekklisia.church.model.Church;
+
+public interface ChurchRepository extends JpaRepository<Church, Long> {
+
+}
