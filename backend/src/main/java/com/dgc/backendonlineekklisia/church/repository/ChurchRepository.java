9fe045a779b1d51e8f970a2506030574bd06f563
Denison G. Corbal <denison.corbal@gmail.com>
2023-08-01 09:15:45 -0300

    basic backend list churches

--- backend/src/main/java/com/dgc/backendonlineekklisia/church/repository/ChurchRepository.java
+++ backend/src/main/java/com/dgc/backendonlineekklisia/church/repository/ChurchRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.dgc.backendonlineekklisia.church.model.Church;
 
 public interface ChurchRepository extends JpaRepository<Church, Long> {
-
 }
