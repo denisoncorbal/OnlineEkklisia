90bf28a855dbf969539389a5b9538259fb8943bf
Denison G. Corbal <denison.corbal@gmail.com>
2023-08-03 04:17:48 -0300

    integration front and back feature delete church

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes, provideRouter, withComponentInputBinding } from '
 import { CreateComponent } from './pages/church/create/create.component';
 import { ListComponent } from './pages/church/list/list.component';
 import { EditComponent } from './pages/church/edit/edit.component';
+import { DeleteComponent } from './pages/church/delete/delete.component';
 
 const routes: Routes = [
   { path: 'church/create', component: CreateComponent },
   { path: 'church/list', component: ListComponent },
-  { path: 'church/edit/:churchId', component: EditComponent }
+  { path: 'church/edit/:churchId', component: EditComponent },
+  { path: 'church/delete/:churchId', component: DeleteComponent },
 ];
 
 @NgModule({
